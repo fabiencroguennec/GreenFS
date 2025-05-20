d6859ea4bcb69c5b0329995b6dab4af7b803baa4
fabiencroguennec <fabien.croguennec@gmail.com>
2025-05-20 10:49:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,30 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = "image-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/black_green.html",
+  "/black_blue.html",
+  "/black_white.html",
+  "/green_blue.html",
+  "/manifest.json",
+  "https://i.postimg.cc/QN6tVdyC/Black.jpg",
+  "https://i.postimg.cc/nrwcW3DB/Black-Blue.jpg",
+  "https://i.postimg.cc/NF0jb35Q/Black-White.jpg",
+  "https://i.postimg.cc/nz2L4FVd/Green.jpg"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
